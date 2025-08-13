133e2b7ffdbebad197cad7db7e483ad3d587469a
Prince <princeundaviya27@gmail.com>
2025-08-13 12:12:54 +0530

    Initial commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import Tilt from 'react-parallax-tilt'
+import profileImage from '../../assets/images/my_photo.jpg'
+
+const About = () => {
+  return (
+    <section id='about' className='py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32'>
+      <div className='flex flex-col-reverse md:flex-row items-center justify-between'>
+        {/* left side */}
+        <div className='md:w-1/2 text-center md:text-left mt-8 items-center'>
+          {/* Greeting */}
+          <h1 className='text-3xl sm:text-5xl md:text-6xl text-white mb-2 leading-tight'>
+            Hi, I am
+          </h1>
+          {/* Name */}
+          <h2 className='tet-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight'>
+            Prince Undaviya
+          </h2>
+          {/* Skills heading with typing effect */}
+          <h3 className='text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight'>
+            <span className='text-white'>
+              I am a
+            </span>
+            <span className='text-[#8245ec] ml-2 text-2xl sm:text-3xl md:text-4xl font-bold'>Frontend Developer</span>
+          </h3>
+          {/* About Me */}
+          <p className='text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relax'>
+            Innovation and driven Computer Engineering student puruuing Bachelor`s degree. Proficient in programming language such as HTML, Javascript, CSS, React.JS, Talwind CSS.
+            I have good communication skills and can work effectively in a team and independently, with strong attention to detail.</p>
+          {/* resume button */}
+          <a href="src\assets\resume\Prince_Udaviya_resume.pdf"
+            target='_blank'
+            rel='noopener noreferrer'
+            className='inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105'
+            style={{
+              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
+              boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
+            }}
+          >DOWNLOAD CV</a>
+        </div>
+
+        {/* Right section */}
+        <div className='md:w-1/2 flex justify-center md:justify-end'>
+          <Tilt 
+          className='w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full'
+          tiltMaxAngleX={20}
+          tiltMaxAngleY={20}
+          perspective={1000}
+          scale={1.05}
+          transitionSpeed={1000}
+          gyroscope={true}
+          >
+            <img src={profileImage} alt="Prince Udaviya" 
+              className='w-full h-full rounded-full object-cover drop-shadow[0_10px_20px_rgb(130,69,236,0.5)]'
+            />
+          </Tilt>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  // Smooth scroll function
+  const handleScroll = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <footer className="text-white py-8 px-[12vw] md:px-[7vw] lg:px-[20vw]">
+      <div className="container mx-auto text-center">
+        {/* Name / Logo */}
+        <h2 className="text-xl font-semibold text-purple-500">Prince Udaviya</h2>
+
+        {/* Navigation Links - Responsive */}
+        <nav className="flex flex-wrap justify-center space-x-4 sm:space-x-6 mt-4">
+          {[
+            { name: "About", id: "about" },
+            { name: "Skills", id: "skills" },
+            { name: "Experience", id: "experience" },
+            { name: "Projects", id: "work" },
+            { name: "Education", id: "education" },
+          ].map((item, index) => (
+            <button
+              key={index}
+              onClick={() => handleScroll(item.id)}
+              className="hover:text-purple-500 text-sm sm:text-base my-1"
+            >
+              {item.name}
+            </button>
+          ))}
+        </nav>
+
+        {/* Social Media Icons - Responsive */}
+        <div className="flex flex-wrap justify-center space-x-4 mt-6">
+          {[          
+            { icon: <FaLinkedin />, link: "https://www.linkedin.com/in/prince-undaviya-36b987271" },
+            { icon: <FaInstagram />, link: "https://www.instagram.com/prince_undaviya/" },
+            {icon: <FaGithub />, link: "https://github.com/Prince-2702"}
+          ].map((item, index) => (
+            <a
+              key={index}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-xl hover:text-purple-500 transition-transform transform hover:scale-110"
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
